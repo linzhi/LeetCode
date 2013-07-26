0e31a83c6a5fdcd322747be89f69956550dc09aa
Linzhi.Qi <qilinzhi@gmail.com>
2013-07-26 19:39:23 +0800

    seventh commit

---
+++ First_Missing_Positive.cpp
@@ -0,0 +1,23 @@
+/*
+ * Author: linzhi
+ *
+ * Title: First Missing Positive
+ *
+ */
+
+class Solution {
+public:
+    int firstMissingPositive(int A[], int n) {
+        for (int i = 0; i < n; i++) {
+            while (A[i] != i + 1 && A[i] >= 1 && A[i] <= n && A[i] != A[A[i] - 1])
+                swap(A[i], A[A[i] - 1]); 
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (A[i] != i + 1) 
+                return i + 1;
+        }
+
+        return n + 1;
+    }
+};
