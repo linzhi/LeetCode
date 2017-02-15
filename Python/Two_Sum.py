267659d89fa133fbdbb207ca433dbb23d8a6f395
Linzhi Qi <qilinzhi@gmail.com>
2017-02-15 11:58:48 +0800

    a new A

---
+++ Python/Two_Sum.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) < 2: return
+        #nums.sort()
+
+        result = []
+        for i in xrange(0, len(nums)):
+            tmp_nums = nums[i + 1:]
+            for j in xrange(0, len(tmp_nums)):
+                if nums[i] + tmp_nums[j] == target:
+                    result.append(i)
+                    result.append(j + i + 1)
+                    break
+        
+        return result
+
+if __name__ == "__main__":
+    demo = Solution()
+    print demo.twoSum([3, 2, 4], 6)
